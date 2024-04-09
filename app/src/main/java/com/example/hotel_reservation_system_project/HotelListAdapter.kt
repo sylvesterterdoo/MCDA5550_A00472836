90fc6f47d29b2c64bdf590c08c38d4a34180d1ec
Sylvester <sylvesterterdoo@gmail.com>
2024-04-09 20:22:54 -0300

    added code to select from list of data and respond

--- app/src/main/java/com/example/hotel_reservation_system_project/HotelListAdapter.kt
+++ app/src/main/java/com/example/hotel_reservation_system_project/HotelListAdapter.kt
@@ -1,3 +1,72 @@
+//package com.example.hotel_reservation_system_project
+//
+//import android.content.Context
+//import android.graphics.Color
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class HotelListAdapter(private val context: Context, private val hotelListData: List<HotelListData>) :
+//    RecyclerView.Adapter<HotelListAdapter.ViewHolder>() {
+//
+//    var selectedPosition = -1;
+//
+//    private var layoutInflater: LayoutInflater = LayoutInflater.from(context)
+//    private var clickListener: ItemClickListener? = null
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val view = layoutInflater.inflate(R.layout.hotel_list_layout, parent, false)
+//        return ViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        if (selectedPosition == position)
+//            holder.itemView.setBackgroundColor(Color.parseColor("#000000"))
+//        else
+//            holder.itemView.setBackgroundColor(Color.parseColor("#ffffff"))
+//        val hotelName = hotelListData[position].name
+//        val hotelPrice = hotelListData[position].price
+//        val hotelAvailability = hotelListData[position].availability
+//
+//        // set up the text
+//        holder.hotelName.text = hotelName
+//        holder.hotelAvailability.text = hotelAvailability
+//        holder.hotelPrice.text = hotelPrice
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return hotelListData.size
+//    }
+//
+//    fun setClickListener(itemClickListener: ItemClickListener) {
+//        clickListener = itemClickListener
+//    }
+//
+//    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+//        var hotelName: TextView = itemView.findViewById(R.id.hotel_name_text_view)
+//        var hotelPrice: TextView = itemView.findViewById(R.id.price_text_view)
+//        var hotelAvailability: TextView = itemView.findViewById(R.id.availability_text_view)
+//
+//        init {
+//            itemView.setOnClickListener(this)
+//        }
+//
+//        override fun onClick(view: View) {
+////            println("Position: ${adapterPosition}")
+////            clickListener?.onClick(view, adapterPosition)
+//            if (clickListener != null) {
+//                val position = adapterPosition
+//                if (position != RecyclerView.NO_POSITION) {
+//                    clickListener?.onClick(view, adapterPosition)
+//                }
+//            }
+//        }
+//    }
+//}
+//
+
 package com.example.hotel_reservation_system_project
 
 import android.content.Context
@@ -11,29 +80,17 @@ import androidx.recyclerview.widget.RecyclerView
 class HotelListAdapter(private val context: Context, private val hotelListData: List<HotelListData>) :
     RecyclerView.Adapter<HotelListAdapter.ViewHolder>() {
 
-    var selectedPosition = -1;
-
-    private var layoutInflater: LayoutInflater = LayoutInflater.from(context)
+    private var selectedPosition = RecyclerView.NO_POSITION
     private var clickListener: ItemClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = layoutInflater.inflate(R.layout.hotel_list_layout, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.hotel_list_layout, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (selectedPosition == position)
-            holder.itemView.setBackgroundColor(Color.parseColor("#000000"))
-        else
-            holder.itemView.setBackgroundColor(Color.parseColor("#ffffff"))
-        val hotelName = hotelListData[position].name
-        val hotelPrice = hotelListData[position].price
-        val hotelAvailability = hotelListData[position].availability
-
-        // set up the text
-        holder.hotelName.text = hotelName
-        holder.hotelAvailability.text = hotelAvailability
-        holder.hotelPrice.text = hotelPrice
+        val hotel = hotelListData[position]
+        holder.bind(hotel, position)
     }
 
     override fun getItemCount(): Int {
@@ -45,23 +102,36 @@ class HotelListAdapter(private val context: Context, private val hotelListData: 
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-        var hotelName: TextView = itemView.findViewById(R.id.hotel_name_text_view)
-        var hotelPrice: TextView = itemView.findViewById(R.id.price_text_view)
-        var hotelAvailability: TextView = itemView.findViewById(R.id.availability_text_view)
+        private val hotelName: TextView = itemView.findViewById(R.id.hotel_name_text_view)
+        private val hotelPrice: TextView = itemView.findViewById(R.id.price_text_view)
+        private val hotelAvailability: TextView = itemView.findViewById(R.id.availability_text_view)
 
         init {
             itemView.setOnClickListener(this)
         }
 
-        override fun onClick(view: View) {
-//            println("Position: ${adapterPosition}")
-//            clickListener?.onClick(view, adapterPosition)
-            if (clickListener != null) {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    clickListener?.onClick(view, adapterPosition)
-                }
+        fun bind(hotel: HotelListData, position: Int) {
+            hotelName.text = hotel.name
+            hotelPrice.text = hotel.price
+            hotelAvailability.text = hotel.availability
+
+            // Highlight selected item
+            if (position == selectedPosition) {
+                itemView.setBackgroundColor(Color.parseColor("#000000"))
+            } else {
+                itemView.setBackgroundColor(Color.parseColor("#ffffff"))
             }
         }
+
+        override fun onClick(view: View) {
+            selectedPosition = adapterPosition
+            notifyDataSetChanged() // Refresh list to update item views
+
+            clickListener?.onClick(view, selectedPosition)
+        }
+    }
+
+    interface ItemClickListener {
+        fun onClick(view: View, position: Int)
     }
 }
--- app/src/main/java/com/example/hotel_reservation_system_project/HotelsListFragment.kt
+++ app/src/main/java/com/example/hotel_reservation_system_project/HotelsListFragment.kt
@@ -1,3 +1,119 @@
+/*
+//package com.example.hotel_reservation_system_project
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.ProgressBar
+//import android.widget.TextView
+//import android.widget.Toast
+//import androidx.fragment.app.Fragment
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
+//import retrofit2.Call
+//import retrofit2.Callback
+//import retrofit2.Response
+//
+//class HotelsListFragment: Fragment(), ItemClickListener {
+//
+//    lateinit var rootView: View
+//    lateinit var headingTextView: TextView
+//    lateinit var progressBar: ProgressBar
+//    lateinit var userListResponseData: List<HotelListData>
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        super.onCreateView(inflater, container, savedInstanceState)
+//        rootView = inflater.inflate(R.layout.hotel_list_fragment, container, false)
+//        return rootView
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        headingTextView = view.findViewById(R.id.heading_text_view)
+//        progressBar = view.findViewById(R.id.progress_bar)
+//
+//        val checkInDate = arguments?.getString(ReservationInfo.CHECK_IN_DATE.value)
+//        val checkOutDate = arguments?.getString(ReservationInfo.CHECK_OUT_DATE.value)
+//        val numberOfGuests = arguments?.getString(ReservationInfo.NUMBER_OF_GUESTS.value)
+//
+//        val tempText = "Welcome user, displaying hotel for $numberOfGuests guests staying from $checkInDate to $checkOutDate"
+//        headingTextView.setText(tempText)
+//
+//        getHotelsListsData()
+//    }
+//
+//    private fun setupRecyclerView() {
+//        progressBar.visibility = View.GONE
+//        val recyclerView: RecyclerView = rootView.findViewById(R.id.hotel_list_recyclerView)
+//        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+//        val hotelListAdapter = HotelListAdapter(requireActivity(), userListResponseData)
+//        recyclerView.adapter = hotelListAdapter
+//
+//        // Bind the click listener
+////        hotelListAdapter.setClickListener()
+//    }
+//
+//    private fun getHotelsListsData() {
+//        progressBar.visibility = View.VISIBLE
+//        Api.getClient().getHotelsLists().enqueue(object : Callback<List<HotelListData>> {
+//            override fun onResponse(call: Call<List<HotelListData>>, response: Response<List<HotelListData>>) {
+//                if (response.isSuccessful) {
+//                    val userListResponses = response.body()
+//                    // Process the response data here
+//                    if (userListResponses != null) {
+//                        userListResponseData = userListResponses
+//                    }
+//
+//                    // Set up the RecyclerView
+//                    setupRecyclerView()
+//                } else {
+//                    // Handle unsuccessful response
+//                    Toast.makeText(requireActivity(), "Failed to fetch hotel lists", Toast.LENGTH_LONG).show()
+//                }
+//                progressBar.visibility = View.GONE
+//            }
+//
+//            override fun onFailure(call: Call<List<HotelListData>>, t: Throwable) {
+//                // Handle network errors
+//                Toast.makeText(requireActivity(), "Network error: ${t.message}", Toast.LENGTH_LONG).show()
+//                progressBar.visibility = View.GONE
+//            }
+//        })
+//    }
+//
+//
+//
+//    override fun onClick(view: View, position: Int) {
+//        Toast.makeText(context, "Item clicked at position " + position, Toast.LENGTH_SHORT).show();
+//    }
+//
+//}
+//
+
+
+//    fun mockHotelListData(): List<HotelListData> {
+//        val list = mutableListOf<HotelListData>()
+//
+//        list.add(HotelListData("Halifax Regional Hotel", "2000$", "true"))
+//        list.add(HotelListData("Hotel Pearl", "500$", "false"))
+//        list.add(HotelListData("Hotel Amano", "800$", "true"))
+//        list.add(HotelListData("San Jones", "250$", "false"))
+//        list.add(HotelListData("Halifax Regional Hotel", "2000$", "true"))
+//        list.add(HotelListData("Hotel Pearl", "500$", "false"))
+//        list.add(HotelListData("Hotel Amano", "800$", "true"))
+//        list.add(HotelListData("San Jones", "250$", "false"))
+//
+//        return list
+//    }
+
+
+*/
+
 package com.example.hotel_reservation_system_project
 
 import android.os.Bundle
@@ -14,19 +130,19 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class HotelsListFragment: Fragment(), ItemClickListener {
+class HotelsListFragment : Fragment(), HotelListAdapter.ItemClickListener {
 
-    lateinit var rootView: View
-    lateinit var headingTextView: TextView
-    lateinit var progressBar: ProgressBar
-    lateinit var userListResponseData: List<HotelListData>
+    private lateinit var rootView: View
+    private lateinit var headingTextView: TextView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var userListResponseData: List<HotelListData>
+    private lateinit var hotelListAdapter: HotelListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
         rootView = inflater.inflate(R.layout.hotel_list_fragment, container, false)
         return rootView
     }
@@ -40,8 +156,8 @@ class HotelsListFragment: Fragment(), ItemClickListener {
         val checkOutDate = arguments?.getString(ReservationInfo.CHECK_OUT_DATE.value)
         val numberOfGuests = arguments?.getString(ReservationInfo.NUMBER_OF_GUESTS.value)
 
-        val tempText = "Welcome user, displaying hotel for $numberOfGuests guests staying from $checkInDate to $checkOutDate"
-        headingTextView.setText(tempText)
+        val tempText = "Welcome user, displaying hotels for $numberOfGuests guests staying from $checkInDate to $checkOutDate"
+        headingTextView.text = tempText
 
         getHotelsListsData()
     }
@@ -50,10 +166,10 @@ class HotelsListFragment: Fragment(), ItemClickListener {
         progressBar.visibility = View.GONE
         val recyclerView: RecyclerView = rootView.findViewById(R.id.hotel_list_recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
-        val hotelListAdapter = HotelListAdapter(requireActivity(), userListResponseData)
+        hotelListAdapter = HotelListAdapter(requireActivity(), userListResponseData)
         recyclerView.adapter = hotelListAdapter
 
-        // Bind the click listener
+        // Set the click listener for item clicks
         hotelListAdapter.setClickListener(this)
     }
 
@@ -64,9 +180,7 @@ class HotelsListFragment: Fragment(), ItemClickListener {
                 if (response.isSuccessful) {
                     val userListResponses = response.body()
                     // Process the response data here
-                    if (userListResponses != null) {
-                        userListResponseData = userListResponses
-                    }
+                    userListResponseData = userListResponses ?: emptyList()
 
                     // Set up the RecyclerView
                     setupRecyclerView()
@@ -85,27 +199,10 @@ class HotelsListFragment: Fragment(), ItemClickListener {
         })
     }
 
-
-
     override fun onClick(view: View, position: Int) {
-        Toast.makeText(context, "Item clicked at position " + position, Toast.LENGTH_SHORT).show();
+        val selectedHotel = userListResponseData[position]
+        Toast.makeText(requireContext(), "Clicked on hotel: ${selectedHotel.name}", Toast.LENGTH_SHORT).show()
+
+        // Perform actions based on the clicked item, e.g., navigate to details screen
     }
-
 }
-
-
-
-//    fun mockHotelListData(): List<HotelListData> {
-//        val list = mutableListOf<HotelListData>()
-//
-//        list.add(HotelListData("Halifax Regional Hotel", "2000$", "true"))
-//        list.add(HotelListData("Hotel Pearl", "500$", "false"))
-//        list.add(HotelListData("Hotel Amano", "800$", "true"))
-//        list.add(HotelListData("San Jones", "250$", "false"))
-//        list.add(HotelListData("Halifax Regional Hotel", "2000$", "true"))
-//        list.add(HotelListData("Hotel Pearl", "500$", "false"))
-//        list.add(HotelListData("Hotel Amano", "800$", "true"))
-//        list.add(HotelListData("San Jones", "250$", "false"))
-//
-//        return list
-//    }
