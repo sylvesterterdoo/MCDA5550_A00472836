223b150d852c8c53610ef07afc66f663bd08c7b9
Sylvester <sylvesterterdoo@gmail.com>
2024-04-13 09:49:50 -0300

    added cloud details

--- app/src/main/java/com/example/hotel_reservation_system_project/Api.kt
+++ app/src/main/java/com/example/hotel_reservation_system_project/Api.kt
@@ -4,13 +4,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Api {
-    val BASE_URL = "http://demoapp-env.eba-nmrqzhsk.us-east-1.elasticbeanstalk.com/"
+    val BASE_URL = "http://apidemo-env.eba-6mnuxd8a.us-east-1.elasticbeanstalk.com/"
     fun getClient(): ApiInterface {
         val retrofit = Retrofit.Builder()
             // Set the Root URL
-            .baseUrl("http://10.0.2.2:8080/")
-//            .baseUrl("http://10.0.2.2:8000/")
-//            .baseUrl(BASE_URL)
+//            .baseUrl("http://10.0.2.2:8080/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()) // Use Gson converter
             .build()
 
